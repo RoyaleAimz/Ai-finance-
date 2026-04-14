afc15757af2c608ec7e5e42776c2286380fbd46e
RoyaleAimz <sycozxhalo@gmail.com>
2026-04-13 20:49:51 -0400

    Create Dashboard component for main page

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1 style={{ fontSize: "22px", marginBottom: "10px" }}>
+        Dashboard
+      </h1>
+
+      <div style={{ marginBottom: "10px" }}>
+        Balance: $0
+      </div>
+
+      <div style={{ marginBottom: "10px" }}>
+        Bills: None
+      </div>
+
+      <div>
+        Income: None
+      </div>
+    </div>
+  );
+}
