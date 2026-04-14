01beb42b08fc04cf5cc419a3ba6bcd31985915cf
RoyaleAimz <sycozxhalo@gmail.com>
2026-04-13 21:15:38 -0400

    Add Navbar component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ background: "#0b0f17", color: "white" }}>
+      <body style={{ background: "#0b0f17", color: "white", margin: 0 }}>
+        <Navbar />
         {children}
       </body>
     </html>
