969e9b54b092d1b110c63d087b4551618c5c31ef
RoyaleAimz <sycozxhalo@gmail.com>
2026-04-14 14:49:46 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,19 @@
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ background: "#0b0f17", color: "white", margin: 0 }}>
+        <Navbar />
+
+        <div style={{ minHeight: "80vh" }}>
+          {children}
+        </div>
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
